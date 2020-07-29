b43842f3caf1faadd0c8061eb7bd66b656b364d9
Pigrabbit <donghyuk1003@gmail.com>
2020-07-29 16:18:31 +0900

    Add payment model

--- server/controller/payment-controller.js
+++ server/controller/payment-controller.js
@@ -1,15 +1,14 @@
+const Payment = require("../model/payment")
+
 class PaymentController {
     constructor() {
 
     }
 
-    getAllPayment(req, res, next) {
-        const result = [
-            {payment_name: "우리카드"},
-            {payment_name: "카카오체크카드"},
-            {payment_name: "국민은행"},
-        ];
-
+    async getAllPayment(req, res, next) {
+        const paymentInstance = new Payment();
+        const result = await paymentInstance.findAll();
+        
         res.json(result);
     }
 }
