a33bb18f67ef185d8c61ee4a95ff1b3fe059cfb3
Pigrabbit <donghyuk1003@gmail.com>
2020-07-29 14:25:13 +0900

    Add cors npm package

--- server/app.js
+++ server/app.js
@@ -1,37 +1,37 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
+app.set("view engine", "html");
 
-app.use(logger('dev'));
+app.use(cors({ origin: "*" }));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 const apiRouter = require("./routes/api-router");
 app.use("/api", apiRouter);
 
 app.use("/", (req, res, next) => {
-  res.send("hello world");
+  res.sendFile("public/index.html", { root: __dirname });
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("running!!!! ")
-}) 
-
-// module.exports = app;
+  console.log("running!!!! ");
+});
