b3544ea74596a6eb8b413c42ef21f3f0e70806ab
Pigrabbit <donghyuk1003@gmail.com>
2020-07-29 14:12:20 +0900

    Add api router

--- server/app.js
+++ server/app.js
@@ -1,17 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+const apiRouter = require("./routes/api-router");
+app.use("/api", apiRouter);
+
+app.use("/", (req, res, next) => {
+  res.send("hello world");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,12 +28,10 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
-module.exports = app;
+app.listen(process.env.PORT || 3000, () => {
+  console.log("running!!!! ")
+}) 
+
+// module.exports = app;
