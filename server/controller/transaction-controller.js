d1ad957983353c882b51d34487b4f252c9f96c3c
Pigrabbit <donghyuk1003@gmail.com>
2020-07-30 17:42:01 +0900

    POST Transaction API 구현

--- server/controller/transaction-controller.js
+++ server/controller/transaction-controller.js
@@ -3,7 +3,18 @@ class TransactionController {
     this.transaction = transaction;
   }
 
-  async createTransaction() {}
+  async createTransaction(req, res, next) {
+    try {
+      const data = req.body;
+      data.username = req.cookies.username;
+    //   res.json(data);
+      await this.transaction.create(data);
+
+      res.status(201).json({ message: "transaction successfully created"});
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = TransactionController;
--- server/model/transaction.js
+++ server/model/transaction.js
@@ -3,13 +3,36 @@ class Transaction {
     this.db = db;
   }
 
-  async create() {
+  async create(data) {
     const conn = await this.db.getConnection();
     try {
+        await conn.beginTransaction();
+
+        let { username, category, content, payment_name, amount, t_type, created_at } = data;
+        // 지출: 0, 수입: 1
+        t_type = (t_type === "지출") ? 0 : 1;
+
+        const getUserIdQuery = "SELECT id as user_id FROM user WHERE username=?";
+        let [rows] = await conn.query(getUserIdQuery, [ username ]);
+        const { user_id } = rows[0];
+        
+        const getPaymentIdQuery = "SELECT id as payment_id FROM payment WHERE payment_name=?";
+        [rows] = await conn.query(getPaymentIdQuery, [ payment_name ]);
+        const { payment_id } = rows[0];
+
+        const insertTxQuery = `INSERT INTO transaction 
+                (category, user_id, payment_id, amount, created_at, content, t_type) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)`;
+
+        await conn.query(insertTxQuery, [ category, user_id, payment_id, amount, created_at, content, t_type]);
+        await conn.commit();
     } catch (error) {
+      conn.rollback();
       throw error;
     } finally {
       conn.release();
     }
   }
 }
+
+module.exports = Transaction;
