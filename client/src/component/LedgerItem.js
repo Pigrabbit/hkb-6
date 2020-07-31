2903739d3b2d6cb148c4672193a92063a877610e
Pigrabbit <donghyuk1003@gmail.com>
2020-07-31 12:40:38 +0900

    일간 수입, 지출 합계 계산
    - 수입, 지출에 따른 스타일 변화

--- client/src/component/LedgerItem.js
+++ client/src/component/LedgerItem.js
@@ -6,13 +6,33 @@ export default function LedgerItem(props, idx) {
 
   function render() {
     const records = getLedgerItemByDate(props.date);
+    // TODO
+    // 레코드에 맞도록 일간 수입, 지출 sum 계산
+    // 레코드의 정렬 맞추기 -> scss
+    // 수입/지출 여부에 따라 스타일(색상) 변경 -> scss
+    // 마우스 오버 이벤트가 생기면 수정 버튼 만들기
+    // 수정 버튼 눌렀을 때 현재 레코드의 내용을 input form에 default로 채워주기
+
+    const incomeRecords = records.filter((record) => record.t_type === "수입");
+    const incomeSum =
+      incomeRecords.length > 0
+        ? incomeRecords.reduce(
+            (acc, cur) => acc + parseInt(cur.amount), 0)
+        : 0;
+
+    const outcomeRecords = records.filter((record) => record.t_type === "지출");
+    const outcomeSum =
+      outcomeRecords.length > 0
+        ? outcomeRecords.reduce(
+            (acc, cur) => acc + parseInt(cur.amount), 0)
+        : 0;
 
     const html = `
       <li class="ledger-item-header">
         <div class="ledger-item-header-date">${props.date}</div>
         <div class="ledger-item-header-amount">
-          <p class="ledger-item-header-amount-income">+0원</p>
-          <p class="ledger-item-header-amount-outcome">-26000원</p>
+          <p class="ledger-item-header-amount-income income-text">+${incomeSum}원</p>
+          <p class="ledger-item-header-amount-outcome outcome-text">${outcomeSum}원</p>
         </div>
       </li>
       ${records
@@ -20,12 +40,24 @@ export default function LedgerItem(props, idx) {
           return `
         <li class="ledger-item-record">
         <div class="ledger-item-record-col">
-          <div class="record-category">${record.category}</div>
+          <div class="record-category ${
+            (record.t_type === "지출") 
+            ? "outcome-element"
+            : "income-element"
+          }">${record.category}</div>
           <div class="record-content">${record.content}</div>
         </div>
         <div class="ledger-item-record-col">
           <div class="record-payment">${record.payment}</div>
-          <div class="record-amount">${record.amount}원</div>
+          <div class="record-amount ${
+            (record.t_type === "지출") 
+            ? "outcome-text"
+            : "income-text"
+          }">${
+            (record.t_type === "수입") 
+            ? "+" + record.amount 
+            : record.amount
+          }원</div>
         </div>
       </li>`;
         })
--- client/src/store.js
+++ client/src/store.js
@@ -16,30 +16,7 @@ export const state = {
   },
 
   ledgerItem: {
-    data: {
-      // "2020-07-30": [
-      //   {
-      //     category: "쇼핑/뷰티",
-      //     content: "미용실",
-      //     payment: "현대카드",
-      //     amount: "-20000",
-      //   },
-      //   {
-      //     category: "쇼핑/뷰티",
-      //     content: "미용실",
-      //     payment: "현대카드",
-      //     amount: "-20000",
-      //   },
-      // ],
-      // "2020-07-29": [
-      //   {
-      //     category: "식품",
-      //     content: "편의점",
-      //     payment: "우리카드",
-      //     amount: "-1000",
-      //   },
-      // ],
-    },
+    data: {},
     listeners: {},
   },
   paymentList: {
