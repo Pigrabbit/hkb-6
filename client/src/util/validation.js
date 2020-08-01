7e09a0bbda4f9fe90c4b5f12f751827890388f1a
haerang <gofkdvjvl@naver.com>
2020-08-01 23:44:56 +0900

    숫자를 콤마붙여서 반환하는 함수 이름 변경

--- client/src/util/validation.js
+++ client/src/util/validation.js
@@ -1,4 +1,4 @@
-export function numberWithCommas(x) {
+export function addCommaToNumber(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
@@ -12,7 +12,7 @@ export function removeComma(x) {
 
 export function attachComma(e) {
   const inputtedString = removeComma(e.target.value);
-  e.target.value = numberWithCommas(inputtedString);
+  e.target.value = addCommaToNumber(inputtedString);
 }
 
 //초기화시킬 타겟 엘레멘트, 메세지를 표시할 타켓 엘레멘트, 메세지
