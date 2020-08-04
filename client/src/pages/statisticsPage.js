ddcbd2fa04e424b6ed6465c909049fe2348ad0e4
haerang <gofkdvjvl@naver.com>
2020-08-04 16:53:30 +0900

    [기능추가] statisticsPage #66 통계 페이지 헤더 추가

--- client/src/pages/statisticsPage.js
+++ client/src/pages/statisticsPage.js
@@ -3,7 +3,18 @@ import Bargraph from "../component/Bargraph";
 export default function StatisticsPage() {
   return `
     <div class="statistics" style="width:100%">
-        This is statistics page
+        <header class="statistics-header">
+        <div>
+        <input type="radio" id="male" name="gender" value="male">
+        <label for="male">카테고리별 지출</label>
+        <input type="radio" id="female" name="gender" value="female">
+        <label for="female">일별 지출</label>
+        </div>
+        <div>
+          <p>이번 달 지출 금액</p>
+          <p class="current-month-total-price">444,790원</p>
+        </div>
+        </header>
         ${Bargraph()}
     </div>
 `;
