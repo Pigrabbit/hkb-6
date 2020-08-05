a75237bf28b49967f501665d4909d3273e42fc83
haerang <gofkdvjvl@naver.com>
2020-08-05 10:24:02 +0900

    [충돌수정] #66 statistics page

--- client/src/pages/statisticsPage.js
+++ client/src/pages/statisticsPage.js
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
 import Bargraph from "../component/Bargraph";
-import StatisticsMenu from "../component/StatisticsMenu";
-
-export default function StatisticsPage() {
-  return `
-    <div class="statistics" style="width:80%">
-        ${StatisticsMenu()}
-        ${Bargraph()}
-    </div>
-`;
-}
-=======
 import Header from "../component/Header";
 import Navbar from "../component/Navbar";
 
 import brocolli from "../images/brocolli.png";
 
 export default function StatisticsPage() {
-
-    return `
+  return `
     ${Header({ isPaymentVisible: false })}
     ${Navbar()}
     <section class="container">
         <div class="statistics">
-            This is statistics page
+        ${StatisticsMenu()}
+        ${Bargraph()}
         </div>
     </section>
     <img id="brocolli" alt="브로콜리" src=${brocolli}/>
     `;
 }
->>>>>>> f8a5560e6be2a7641e091f013c22d8345124dd26
