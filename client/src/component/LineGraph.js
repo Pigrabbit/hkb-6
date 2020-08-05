3ed1cc8831a47909663297dde88639b188e36997
haerang <gofkdvjvl@naver.com>
2020-08-05 16:28:08 +0900

    [기능추가] line graph #70 기초 ui작성

--- client/src/component/LineGraph.js
+++ client/src/component/LineGraph.js
@@ -1,46 +1,67 @@
 import "./LineGraph.scss";
+import { getStatisticsData, getCurrentDate, subscribe } from "../store";
 
 export default function LineGraph() {
-  const componentName = "lineGraph";
+  const componentName = "linegraph";
 
   function render() {
     const statistics = getStatisticsData();
+    const { year, month } = getCurrentDate();
+    const lastDay = new Date(year, month, 0).toString().split(" ")[2];
+    const iter = lastDay % 5 === 0 ? lastDay / 5 : parseInt(lastDay / 5) + 1;
     const html = `
-      <figcaption>별 지출</figcaption>
-      <ul data-fesa-num="${statistics.length}">
-      ${statistics
-        .map((item, idx) => {
-          const color = colorPalette[idx % colorPalette.length];
-          return `<li>
-        <div class="bar-category">${item.category}</div>
-        <div class="bar-percentage">${item.percentage} %</div>
-        <div class="bar-graph">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="100%" height="40" aria-labelledby="title" role="img">
-        <g class="bar">
-        <rect y="10" width="${item.percentage * 3}" height="20">
-          <animate attributeName="width" from="0" to ="${
-            item.percentage * 3
-          }" dur="1s" fill="freeze"/>
-          </rect>
-          <text x="${item.percentage * 3 + 20}" y="25">${item.category}</text>
-        </g>
-        </svg>
-        </div>
-        <div class="bar-price">${addCommaToNumber(item.totalPrice)} 원</div>
-        </li>`;
-        })
+      <figcaption>일별 지출</figcaption>
+      <svg version="1.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="graph" aria-labelledby="title" role="img">
+    <g class="grid x-grid" id="xGrid">
+      <line x1="90" x2="90" y1="5" y2="370"></line>
+    </g>
+    <g class="grid y-grid" id="yGrid">
+      <line x1="90" x2="760" y1="370" y2="370"></line>
+    </g>
+      <g class="labels x-labels">
+      ${Array(parseInt(iter))
+        .fill()
+        .map(
+          (item, i) =>
+            `<text x="${110 + i * (650 / (lastDay / 5))}" y="400">${month}월${
+              5 * i + 1
+            }일</text>`
+        )
         .join("")}
-      </ul>
-    
+      <text x="400" y="440" class="label-title">날짜</text>
+    </g>
+    <g class="labels y-labels">
+    ${Array(10)
+      .fill()
+      .map((item, i) => `<text x="80" y="${370 - i * 36.5}">${5 * i}만</text>`)
+      .join("")}
+  <text x="30" y="200" class="label-title">금액</text>
+    </g>
+    <g class="data" data-setname="Our first data set">
+        <circle cx="90" cy="192" data-value="7.2" r="4"></circle>
+        <circle cx="240" cy="141" data-value="8.1" r="4"></circle>
+        <circle cx="388" cy="179" data-value="7.7" r="4"></circle>
+        <circle cx="531" cy="200" data-value="6.8" r="4"></circle>
+        <circle cx="677" cy="104" data-value="6.7" r="4"></circle>
+    </g>
+    <polyline
+    fill="none"
+    stroke="#0074d9"
+    stroke-width="3"
+    points="
+      90,192,
+      240,141
+      388,179
+      531,200,
+      677,104"/>
+    </svg>
     
       `;
 
     const $header = document.querySelector(`.${componentName}`);
     $header.innerHTML = html;
-
-    // bindEvent("button.header-payment-btn", "click", onPaymentBtnClick);
   }
-
+  subscribe(componentName, "currentDate", render);
   setTimeout(render, 0);
 
   return `<figure class=${componentName}></figure>`;
--- client/src/pages/statisticsPage.js
+++ client/src/pages/statisticsPage.js
@@ -1,9 +1,10 @@
 import Bargraph from "../component/Bargraph";
 import Header from "../component/Header";
 import Navbar from "../component/Navbar";
+import LineGraph from "../component/LineGraph";
+import StatisticsMenu from "../component/StatisticsMenu";
 
 import brocolli from "../images/brocolli.png";
-import StatisticsMenu from "../component/StatisticsMenu";
 
 export default function StatisticsPage() {
   return `
@@ -13,6 +14,7 @@ export default function StatisticsPage() {
         <div class="statistics">
         ${StatisticsMenu()}
         ${Bargraph()}
+        ${LineGraph()}
         </div>
     </section>
     <img id="brocolli" alt="브로콜리" src=${brocolli}/>
