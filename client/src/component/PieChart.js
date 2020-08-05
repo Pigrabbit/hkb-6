54ee1acfd6d63413c4e41e7f3e7a079775953e98
Pigrabbit <donghyuk1003@gmail.com>
2020-08-05 17:38:14 +0900

    [기능추가]/PieChart 라벨 추가

--- client/src/component/PieChart.js
+++ client/src/component/PieChart.js
@@ -10,83 +10,96 @@ export default function PieChart() {
     const statisticsData = getStatisticsData();
     console.log(statisticsData);
 
-    const radius = 10;
+    // SVG 관련 상수
+    const radius = 30;
     const pi = 3.14;
     const circumference = 2 * pi * radius;
+    const outerRadius = 45;
 
-    // 오름차순 정렬
-    const percentagesNew = Object.values(statisticsData)
-        .sort((a, b) => (parseInt(a.percentage) > parseInt(b.percentages)) ? 1 : -1);
-    console.log(percentagesNew)
+    // 차트 그리기
+    // 누적 percentage 구하기
+    // const ascendingPercentages = statisticsData.map(item => parseInt(item.percentage))
+    // .sort((a, b) => a - b);
+    // ascendingPercentages.push(100);
+    // console.log(ascendingPercentages);
+    const percentages = statisticsData.map(item => parseInt(item.percentage));
+    percentages.push(100 - percentages.reduce((acc, cur) => acc + cur));
+    console.log(percentages);
 
-    const accumulatedPercentages = percentagesNew.map(( _, idx, arr) => {
-        return arr.slice(idx).reduce((acc, cur) => acc + parseInt(cur.percentage), 0);
-    });
+    const accumulatedPercentages = Object.values(statisticsData)
+      .sort((a, b) =>
+        parseInt(a.percentage) > parseInt(b.percentages) ? 1 : -1
+      )
+      .map((_, idx, arr) => {
+        return arr
+          .slice(idx)
+          .reduce((acc, cur) => acc + parseInt(cur.percentage), 0);
+      });
+
     accumulatedPercentages.unshift(100);
     console.log(accumulatedPercentages);
-    
 
-    const categories = statisticsData.map(item => item.category);
+    const categories = statisticsData.map((item) => item.category);
     categories.push("기타");
+    categories.reverse();
     console.log(categories);
-    const reversed = categories.reverse();
-    console.log(reversed);
 
-    const categoryEng = {
-        "생활": "life",
-        "식비": "grocery",
-        "의료/건강": "health",
-        "쇼핑/뷰티": "shopping",
-        "교통": "transportation",
-        "기타": "etc",
-    };
+    // 라벨 넣기
+
+    const labelCoords = [];
+    const degrees = [];
+    percentages.forEach((percentage, idx) => {
+      let theta;
+
+      if (idx === 0) {
+        theta = 360 * (percentage / 100);
+        degrees.push(theta);
+        theta = theta / 2;
+      } else {
+        const prevTheta = degrees.pop();
+        const curTheta = 360 * (percentage / 100);
+
+        degrees.push(prevTheta + curTheta);
+        theta = prevTheta + (curTheta / 2);
+      }
+
+    //   console.log(theta);
+      labelCoords.unshift(labelCoords[idx] = {
+        x: Math.sin((theta) * (pi / 180)) * outerRadius,
+        y: -Math.cos((theta) * (pi / 180)) * outerRadius,
+      })
+    //   labelCoords[idx] = {
+    //     x: Math.sin((theta) * (pi / 180)) * outerRadius,
+    //     y: -Math.cos((theta) * (pi / 180)) * outerRadius,
+    //   };
+    });
+
+    console.log(labelCoords);
 
     const html = `
-    <svg class="pie-chart-svg" viewBox="0 0 40 40">
-        ${accumulatedPercentages.map((percentage, idx) => {
+    <svg class="pie-chart-svg" viewBox="-50 -50 100 100">
+        ${accumulatedPercentages
+          .map((percentage, idx) => {
             return `
-                <circle class="${categoryEng[categories[idx]]} target" r="10" cx="20" cy="20" fill="transparent"
+                <circle class="${categories[idx]} target" r="30" cx="0" cy="0" fill="transparent"
                 stroke-width="20"
                 stroke-dasharray="calc((${percentage}/100) * ${circumference}) ${circumference}"
-                transform="rotate(-90) translate(-40)"/>`;
-        }).join("")}
-        <circle class="inner" r="10" cx="20" cy="20" fill="white"/>
+                transform="rotate(-90)"/>`;
+          })
+          .join("")}
+          ${categories
+            .map((category, idx) => {
+              return `
+                <text class="${category} label" x=${labelCoords[idx].x} y=${labelCoords[idx].y}>${category}</text>
+            `;
+            })
+            .join("")}
+        <circle class="inner" r="10" cx="0" cy="0" fill="white"/>
     </svg>
     `;
 
     const $pieChart = $(`.${componentName}`);
     $pieChart.innerHTML = html;
-
-    // const $pies = [...document.querySelectorAll("circle.target")];
-
-    // const data = {
-    //   life: 71,
-    //   grocery: 16,
-    //   transportation: 4,
-    // };
-
-    // const percentages = Object.values(data)
-    //   .sort((a, b) => a - b) // 오름차순 정렬
-    //   .map((num, idx, arr) => {
-    //     // 자기 보다 뒤에 나오는 element 모두 sum
-    //     return arr.slice(idx).reduce((acc, cur) => acc + cur);
-    //   });
-
-    // $pies.forEach((pie, idx) => {
-    //   if (idx === 0) {
-    //     pie.setAttribute(
-    //       "stroke-dasharray",
-    //       `${circumference} ${circumference}`
-    //     );
-    //   } else {
-    //     pie.setAttribute(
-    //       "stroke-dasharray",
-    //       `calc((${
-    //         percentages[idx - 1]
-    //       }/100) * ${circumference}) ${circumference}`
-    //     );
-    //   }
-    // });
   }
 
   setTimeout(render, 0);
