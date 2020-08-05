4f6b5be14f41d34cbc97f31532a0181bee772238
haerang <gofkdvjvl@naver.com>
2020-08-05 14:20:09 +0900

    [리팩토링] bar-graph #66 bar graph 랜덤 색깔 지정 코드 수정

--- client/src/component/LineGraph.js
+++ client/src/component/LineGraph.js
@@ -1,29 +1,23 @@
-import "./Bargraph.scss";
-import { getStatisticsData } from "../store";
-import { addCommaToNumber } from "../util/validation";
-import { getRandomColor } from "../util/util";
+import "./LineGraph.scss";
 
-export default function Bargraph() {
-  const componentName = "bargraph";
+export default function LineGraph() {
+  const componentName = "lineGraph";
 
   function render() {
     const statistics = getStatisticsData();
-
     const html = `
-      <figcaption>카테고리별 지출</figcaption>
-      <ul>
+      <figcaption>별 지출</figcaption>
+      <ul data-fesa-num="${statistics.length}">
       ${statistics
         .map((item, idx) => {
-          const color = getRandomColor();
+          const color = colorPalette[idx % colorPalette.length];
           return `<li>
         <div class="bar-category">${item.category}</div>
         <div class="bar-percentage">${item.percentage} %</div>
         <div class="bar-graph">
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="100%" height="40" aria-labelledby="title" role="img">
         <g class="bar">
-        <rect y="10" width="${
-          item.percentage * 3
-        }" height="20" fill="#${color}">
+        <rect y="10" width="${item.percentage * 3}" height="20">
           <animate attributeName="width" from="0" to ="${
             item.percentage * 3
           }" dur="1s" fill="freeze"/>
@@ -47,7 +41,7 @@ export default function Bargraph() {
     // bindEvent("button.header-payment-btn", "click", onPaymentBtnClick);
   }
 
-  setTimeout(render, 1000);
+  setTimeout(render, 0);
 
   return `<figure class=${componentName}></figure>`;
 }
