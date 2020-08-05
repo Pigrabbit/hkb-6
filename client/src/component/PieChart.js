91e7f6b28b261bde177ec9271ff66a0525b5c640
Pigrabbit <donghyuk1003@gmail.com>
2020-08-05 17:51:41 +0900

    [리팩토링]/PieChart 상수 분리

--- client/src/component/PieChart.js
+++ client/src/component/PieChart.js
@@ -1,89 +1,83 @@
 import "./PieChart.scss";
 
-import { $ } from "../util/util";
+import { $, toRadian } from "../util/util";
 import { getStatisticsData } from "../store";
+import {
+  PIECHART_RADIUS,
+  PIECHART_CIRCUMFERENCE,
+  PIECHART_OUTER_RADIUS,
+} from "../util/constant";
 
 export default function PieChart() {
   const componentName = "piechart";
 
+  const categories = statisticsData.map((item) => item.category);
+  categories.push("기타");
+  categories.reverse();
+
   function render() {
     const statisticsData = getStatisticsData();
-    console.log(statisticsData);
 
-    // SVG 관련 상수
-    const radius = 30;
-    const pi = 3.14;
-    const circumference = 2 * pi * radius;
-    const outerRadius = 45;
+    function getAccumulatedPecentages(statisticsData) {
+      const accumulatedPercentages = Object.values(statisticsData)
+        .sort((a, b) =>
+          parseInt(a.percentage) > parseInt(b.percentages) ? 1 : -1
+        )
+        .map((_, idx, arr) => {
+          return arr
+            .slice(idx)
+            .reduce((acc, cur) => acc + parseInt(cur.percentage), 0);
+        });
+      accumulatedPercentages.unshift(100);
+      return accumulatedPercentages;
+    }
+    
+    const accumulatedPercentages = getAccumulatedPecentages(statisticsData);
 
-    // 차트 그리기
-    // 누적 percentage 구하기
-    // const ascendingPercentages = statisticsData.map(item => parseInt(item.percentage))
-    // .sort((a, b) => a - b);
-    // ascendingPercentages.push(100);
-    // console.log(ascendingPercentages);
-    const percentages = statisticsData.map(item => parseInt(item.percentage));
-    percentages.push(100 - percentages.reduce((acc, cur) => acc + cur));
-    console.log(percentages);
+    function getLabelCoords() {
+      const labelCoords = [];
+      const degrees = [];
 
-    const accumulatedPercentages = Object.values(statisticsData)
-      .sort((a, b) =>
-        parseInt(a.percentage) > parseInt(b.percentages) ? 1 : -1
-      )
-      .map((_, idx, arr) => {
-        return arr
-          .slice(idx)
-          .reduce((acc, cur) => acc + parseInt(cur.percentage), 0);
+      const percentages = statisticsData.map((item) =>
+        parseInt(item.percentage)
+      );
+      const etcPercent = 100 - percentages.reduce((acc, cur) => acc + cur);
+      percentages.push(etcPercent);
+      percentages.forEach((percentage, idx) => {
+        let theta;
+
+        if (idx === 0) {
+          theta = 360 * (percentage / 100);
+          degrees.push(theta);
+          theta = theta / 2;
+        } else {
+          const prevTheta = degrees.pop();
+          const curTheta = 360 * (percentage / 100);
+
+          degrees.push(prevTheta + curTheta);
+          theta = prevTheta + curTheta / 2;
+        }
+
+        labelCoords.unshift(
+          (labelCoords[idx] = {
+            x: Math.sin(toRadian(theta)) * PIECHART_OUTER_RADIUS,
+            y: -Math.cos(toRadian(theta)) * PIECHART_OUTER_RADIUS,
+          })
+        );
       });
+      return labelCoords;
+    }
 
-    accumulatedPercentages.unshift(100);
-    console.log(accumulatedPercentages);
-
-    const categories = statisticsData.map((item) => item.category);
-    categories.push("기타");
-    categories.reverse();
-    console.log(categories);
-
-    // 라벨 넣기
-
-    const labelCoords = [];
-    const degrees = [];
-    percentages.forEach((percentage, idx) => {
-      let theta;
-
-      if (idx === 0) {
-        theta = 360 * (percentage / 100);
-        degrees.push(theta);
-        theta = theta / 2;
-      } else {
-        const prevTheta = degrees.pop();
-        const curTheta = 360 * (percentage / 100);
-
-        degrees.push(prevTheta + curTheta);
-        theta = prevTheta + (curTheta / 2);
-      }
-
-    //   console.log(theta);
-      labelCoords.unshift(labelCoords[idx] = {
-        x: Math.sin((theta) * (pi / 180)) * outerRadius,
-        y: -Math.cos((theta) * (pi / 180)) * outerRadius,
-      })
-    //   labelCoords[idx] = {
-    //     x: Math.sin((theta) * (pi / 180)) * outerRadius,
-    //     y: -Math.cos((theta) * (pi / 180)) * outerRadius,
-    //   };
-    });
-
-    console.log(labelCoords);
+    const labelCoords = getLabelCoords();
 
     const html = `
     <svg class="pie-chart-svg" viewBox="-50 -50 100 100">
         ${accumulatedPercentages
           .map((percentage, idx) => {
             return `
-                <circle class="${categories[idx]} target" r="30" cx="0" cy="0" fill="transparent"
+                <circle class="${categories[idx]} target" r=${PIECHART_RADIUS} cx="0" cy="0" fill="transparent"
                 stroke-width="20"
-                stroke-dasharray="calc((${percentage}/100) * ${circumference}) ${circumference}"
+                stroke-dasharray="calc((${percentage}/100) * ${PIECHART_CIRCUMFERENCE}) ${PIECHART_CIRCUMFERENCE}"
                 transform="rotate(-90)"/>`;
           })
           .join("")}
