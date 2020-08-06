e22f766b551e45a0563eb3ede2b137b33742fb4d
haerang <gofkdvjvl@naver.com>
2020-08-06 15:24:17 +0900

    [기능추가] line graph #70 데이터 라벨 추가 및 예외 데이터 처리
    - 45만원 이상인 데이터 들은 라벨로 얼마인지 명시적으로 보여주고 일정 높이 이상 그래프 범위를 넘어가지 않게 처리해주었습니다.

--- client/src/component/LineGraph.js
+++ client/src/component/LineGraph.js
@@ -16,11 +16,9 @@ export default function LineGraph() {
     const iter = lastDay % 5 === 0 ? lastDay / 5 : parseInt(lastDay / 5) + 1;
 
     const html = `
-    <div style="margin:-20px" class="${
-      getCategoryRadioChecked() ? "hidden" : ""
-    }">
+    <div class="${getCategoryRadioChecked() ? "hidden" : ""}">
       <figcaption style="margin:0">일별 지출</figcaption>
-      <svg version="1.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="graph" aria-labelledby="title" role="img">
+      <svg viewBox="0 0 800 800" version="1.2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="graph" aria-labelledby="title" role="img">
     <g class="grid x-grid" id="xGrid">
       <line x1="90" x2="90" y1="5" y2="370"></line>
     </g>
@@ -37,7 +35,7 @@ export default function LineGraph() {
             }일</text>`
         )
         .join("")}
-      <text x="400" y="440" class="label-title">날짜</text>
+      <text x="440" y="440" class="label-title">날짜</text>
     </g>
     <g class="labels y-labels grid-line">
     ${Array(10)
@@ -50,27 +48,7 @@ export default function LineGraph() {
       )
       .join("")}
   <text x="30" y="200" class="label-title">금액</text>
-    </g>
-    <g class="data" data-setname="Our first data set">
-    ${Array(+lastDay)
-      .fill()
-      .map((item, i) => {
-        const date = `${year}-${month < 10 ? `0${month}` : month}-${
-          i < 9 ? `0${i + 1}` : i + 1
-        }`;
-        let records = getLedgerItemByDate(date);
-        let outcomeSum = 0;
-        if (records) {
-          outcomeSum = getDailyOutcomeSum(records);
-        }
-        const unit = outcomeSum / 10000 / 5;
-        return `<circle cx="${110 + i * (650 / lastDay)}" cy="${
-          370 - unit * 36.5
-        }" data-value="${outcomeSum}" r="4"/>`;
-      })}
-    </g>
-   
-    <polyline
+  <polyline
     fill="none"
     stroke="#0074d9"
     stroke-width="3"
@@ -86,9 +64,49 @@ export default function LineGraph() {
           outcomeSum = getDailyOutcomeSum(records);
         }
         const unit = outcomeSum / 10000 / 5;
-        return `${110 + i * (650 / lastDay)}, ${370 - unit * 36.5}`;
+        if (unit <= 9)
+          return `${110 + i * (650 / lastDay)}, ${370 - unit * 36.5}`;
+        else return `${110 + i * (650 / lastDay)}, ${370 - 9.5 * 36.5}`;
       })}
       "/>
+    </g>
+    <g class="data" data-setname="Our first data set">
+    ${Array(+lastDay)
+      .fill()
+      .map((item, i) => {
+        const date = `${year}-${month < 10 ? `0${month}` : month}-${
+          i < 9 ? `0${i + 1}` : i + 1
+        }`;
+        let records = getLedgerItemByDate(date);
+        let outcomeSum = 0;
+        if (records) {
+          outcomeSum = getDailyOutcomeSum(records);
+        }
+        const unit = outcomeSum / 10000 / 5;
+        return `${
+          unit <= 9
+            ? `<circle cx="${110 + i * (650 / lastDay)}" cy="${
+                370 - unit * 36.5
+              }" data-value="${outcomeSum}" r="4"/>`
+            : `<circle cx="${110 + i * (650 / lastDay)}" cy="${
+                370 - 9.5 * 36.5
+              }" data-value="${outcomeSum}" r="4"/>`
+        }
+        ${
+          outcomeSum === 0
+            ? ""
+            : outcomeSum > 450000
+            ? `<text class="amount-label" x="${90 + i * (650 / lastDay)}" y="${
+                350 - 9.2 * 36.5
+              }">
+      ${parseInt(outcomeSum / 10000)} 만원</text>`
+            : `<text class="amount-label" x="${90 + i * (650 / lastDay)}" y="${
+                350 - unit * 36.5
+              }">
+${parseInt(outcomeSum / 10000)} 만원</text>`
+        }`;
+      })}
+    </g>
     </svg>
     </div>
       `;
